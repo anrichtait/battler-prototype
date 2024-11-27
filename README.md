6fd95c229448cf66650b6e9ad64a9670eed8950d
Anrich Tait <anrichjtait@gmail.com>
2024-11-27 10:55:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Summary
+As part of my computer science and game development studies I am making a prototype for a pokemon LIKE battler.
+The code will be written in rust using the macroquad framework.
+
+_Note: This is a prototype and does not intend to be a full featured game._
+
+# Scope
+List of things I will need to implement for the prototype to be considered "complete":
+- [ ] Atleast one playable level/area.
+- [ ] World triggers (i.e battles, item pickup, etc)
+- [ ] Battle system with different types
+- [ ] Basic animations and tiles
+- [ ] Save System
+
+
+# Resources
+## Assets
+Assets in this project are either made by myself or are freely distributed.
+
+## Some of the current art:
+
+
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use macroquad::experimental::animation::*;
+use macroquad::prelude::*;
+use miniquad::window::set_window_size;
+use macroquad::ui::root_ui;
+
+
+#[macroquad::main("fakemon")]
+async fn main() {
+
+    set_window_size(720, 500);
+
+    let mut william = AnimatedSprite::new(
+        64,
+        64,
+        &[ Animation {
+            name: "idle".to_string(),
+            row: 0,
+            frames: 5,
+            fps: 12,
+        },
+        Animation {
+            name: "walk_south".to_string(),
+            row: 1,
+            frames: 6,
+            fps: 12,
+        },
+        Animation {
+            name: "walk_north".to_string(),
+            row: 2,
+            frames: 6,
+            fps: 12,
+        },
+        Animation {
+            name: "walk_east".to_string(),
+            row: 3,
+            frames: 6,
+            fps: 12,
+        },
+        Animation {
+            name: "walk_west".to_string(),
+            row: 4,
+            frames: 6,
+            fps: 12,
+        }
+        ], true);
+
+    let william_texture = load_texture("assets/william.png").await.unwrap();
+    let mut william_pos: Vec2 = Vec2::new(50.0, 50.0);
+
+    loop {
+        clear_background(DARKGRAY);
+
+        if is_key_down(KeyCode::S) {
+            william_pos.y += 2.0;
+            william.set_animation(1);
+        } else if is_key_down(KeyCode::W) {
+            william_pos.y -= 2.0;
+            william.set_animation(2);
+        } else if is_key_down(KeyCode::D) {
+            william_pos.x += 2.0;
+            william.set_animation(3);
+        } else if is_key_down(KeyCode::A) {
+            william_pos.x -= 2.0;
+            william.set_animation(4);
+        }
+        else {
+            william.set_animation(0);
+        }
+
+        draw_texture_ex(
+            &william_texture,
+            william_pos.x,
+            william_pos.y,
+            WHITE,
+            DrawTextureParams {
+                source: Some(william.frame().source_rect),
+                dest_size: Some(william.frame().dest_size),
+                ..Default::default()
+            },
+        );
+
+        william.update();
+
+        root_ui().label(None, "");
+        if root_ui().button(None, "Push me") {
+            println!("pushed");
+        }
+
+        next_frame().await;
+    }
+}
